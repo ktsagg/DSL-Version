97e002267fd66fe0b42909178bf7632611f91f5e
ktsagg <ktsagg@gmail.com>
2024-09-27 14:52:46 +0300

    major refactoring

--- JoinLexico.py
+++ JoinLexico.py
@@ -1,40 +1,87 @@
 import re
 
+# lemma = "es-gr-lemma.txt"
+# accento = "es-gr-accento.txt"
+# definizioni = "es-gr-definizioni.txt"
 
-def parse_and_join_data(lemma, accent, definition):
-    a = open(lemma, "r", encoding="utf-8").readlines()
-    b = open(accent, "r", encoding="utf-8").readlines()
-    c = open(definition, "r", encoding="utf-8").readlines()
-
-    corpus = []
-    for lem, acc, dfn in zip(a, b, c):
-        concatenated = f"{lem.strip()}\n\t{acc.strip()} {dfn.strip()}\n"
-        corpus.append(replace_characters(concatenated))
-    return corpus
+# lemma_list = open(lemma, "r", encoding="utf-8").readlines()
+# accento_list = open(accento, "r", encoding="utf-8").readlines()
+# definizioni_list = open(definizioni, "r", encoding="utf-8").readlines()
 
 
-def color_numbers(text):
+def read_files(
+    lemmafile="es-gr-lemma.txt",
+    accentofile="es-gr-accento.txt",
+    definizionifile="es-gr-definizioni.txt",
+):
+    lemma_list = open(lemmafile, "r", encoding="utf-8").readlines()
+    accento_list = open(accentofile, "r", encoding="utf-8").readlines()
+    definizioni_list = open(definizionifile, "r", encoding="utf-8").readlines()
+    return lemma_list, accento_list, definizioni_list
+
+
+# Παρέμβαση Κώστας
+def format_accento_list(accento_list: list):
+    return [format_accento_string(accento) for accento in accento_list]
+
+
+def format_accento_string(input_string):
+    pattern = r"\[(.*?)\]"
+    replacement = r"/[i][c maroon]\1[/c][/i]/"
+    output_string = re.sub(pattern, replacement, input_string)
+    return output_string
+
+
+def format_definizione_list(definizioni_list: list):
+    return [format_definizione_string(definizione) for definizione in definizioni_list]
+
+
+def format_definizione_string(input_string):
     pattern = r"(\d{1,2})\."
     replacement = r"[b][c brown]\1.[/c][/b]"
-    return re.sub(pattern, replacement, text)
+    return re.sub(pattern, replacement, input_string)
 
 
-def replace_characters(text_line):
-    new_text = text_line.replace("[", "{")
-    new_text = new_text.replace("]", "}")
-    new_text = color_numbers(new_text)
-    return new_text
+# def format_file(text_line):
+#     new_text = text_line.replace("[", "/")
+#     new_text = new_text.replace("]", "/")
+#     pattern = r"(\d{1,2})\."
+#     replacement = r"[b][c brown]\1.[/c][/b]"
+#     new_text = re.sub(pattern, replacement, new_text)
+#     return new_text
 
 
-def create_dsl(filename: str, corpus: str):
-    header = '#NAME "Español-Griego"\n#INDEX_LANGUAGE "Italian"\n#CONTENTS_LANGUAGE "Greek"\n'
-    complete = header + corpus
-    with open(f"{filename}.dsl", "w", encoding="utf-8") as out:
+# def parse_and_join_data():
+#     # Παρέμβαση Κώστας
+#     accento_list = format_accento_list()
+
+#     for lem, acc, dfn in zip(lemma_list, accento_list, definizioni_list):
+#         concatenated = f"{lem.strip()}\n\t{acc.strip()} {dfn.strip()}\n"
+#         corpus.append(concatenated)
+#         # corpus.append(format_file(concatenated))
+#     return corpus
+
+
+def create_file(file_name: str, corpus: str):
+    FILE_HEADER = '#NAME "Español-Griego"\n#INDEX_LANGUAGE "Italian"\n#CONTENTS_LANGUAGE "Greek"\n'
+    complete = FILE_HEADER + corpus
+    with open(f"{file_name}.dsl", "w", encoding="utf-8") as out:
         out.write(complete)
 
 
+def main():
+    corpus = []
+    lemma_list, accento_list, definizioni_list = read_files()
+    accento_list_formatted = format_accento_list(accento_list)
+    definizioni_list_formatted = format_definizione_list(definizioni_list)
+    for lem, acc, dfn in zip(
+        lemma_list, accento_list_formatted, definizioni_list_formatted
+    ):
+        concatenated = f"{lem.strip()}\n\t{acc.strip()} {dfn.strip()}\n"
+        corpus.append(concatenated)
+    return corpus
+
+
 if __name__ == "__main__":
-    corpus = parse_and_join_data(
-        "esgr-lemma.txt", "esgr-accento.txt", "esgr-definizioni.txt"
-    )
-    create_dsl("Dsl-EsGr", "".join(corpus[1:]))
+    corpus = main()  # parse_and_join_data()
+    create_file("es-gr-dls", "".join(corpus[1:]))
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,12 +1,16 @@
-from JoinLexico import color_numbers
+from JoinLexico import format_accento_string, format_definizione_string
 
 
-def test_replace_numbers_with_div():
-    assert color_numbers("1.") == "[b][c brown]1.[/c][/b]"
-    assert color_numbers("2.") == "[b][c brown]2.[/c][/b]"
-    assert color_numbers("3.") == "[b][c brown]3.[/c][/b]"
-    print(color_numbers("Hello 4. test 12."))
+def test_format_accento_list():
+    assert format_accento_string("[abc adss] ") == "/[i][c maroon]abc adss[/c][/i]/ "
+    assert format_accento_string("[aaa bbb] ") == "/[i][c maroon]aaa bbb[/c][/i]/ "
+
+
+def test_format_definizione_string():
+    assert format_definizione_string("1.") == "[b][c brown]1.[/c][/b]"
+    assert format_definizione_string("2.") == "[b][c brown]2.[/c][/b]"
+    assert format_definizione_string("13.") == "[b][c brown]13.[/c][/b]"
     assert (
-        color_numbers("Hello 4. test 12.")
+        format_definizione_string("Hello 4. test 12.")
         == "Hello [b][c brown]4.[/c][/b] test [b][c brown]12.[/c][/b]"
     )
