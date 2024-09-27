9d729ecebecf33a1bf89cd101e194bf90e86db70
ktsagg <ktsagg@gmail.com>
2024-09-27 15:12:04 +0300

    minor fixes

--- JoinLexico.py
+++ JoinLexico.py
@@ -1,22 +1,13 @@
 import re
 
-# lemma = "es-gr-lemma.txt"
-# accento = "es-gr-accento.txt"
-# definizioni = "es-gr-definizioni.txt"
 
-# lemma_list = open(lemma, "r", encoding="utf-8").readlines()
-# accento_list = open(accento, "r", encoding="utf-8").readlines()
-# definizioni_list = open(definizioni, "r", encoding="utf-8").readlines()
-
-
-def read_files(
-    lemmafile="es-gr-lemma.txt",
-    accentofile="es-gr-accento.txt",
-    definizionifile="es-gr-definizioni.txt",
-):
-    lemma_list = open(lemmafile, "r", encoding="utf-8").readlines()
-    accento_list = open(accentofile, "r", encoding="utf-8").readlines()
-    definizioni_list = open(definizionifile, "r", encoding="utf-8").readlines()
+def read_files(lemmafile, accentofile, definizionifile):
+    lemma_list = open(lemmafile, encoding="utf-8").readlines()
+    lemma_list[0] = lemma_list[0][1:]
+    accento_list = open(accentofile, encoding="utf-8").readlines()
+    accento_list[0] = accento_list[0][1:]
+    definizioni_list = open(definizionifile, encoding="utf-8").readlines()
+    definizioni_list[0] = definizioni_list[0][1:]
     return lemma_list, accento_list, definizioni_list
 
 
@@ -42,36 +33,18 @@ def format_definizione_string(input_string):
     return re.sub(pattern, replacement, input_string)
 
 
-# def format_file(text_line):
-#     new_text = text_line.replace("[", "/")
-#     new_text = new_text.replace("]", "/")
-#     pattern = r"(\d{1,2})\."
-#     replacement = r"[b][c brown]\1.[/c][/b]"
-#     new_text = re.sub(pattern, replacement, new_text)
-#     return new_text
-
-
-# def parse_and_join_data():
-#     # Παρέμβαση Κώστας
-#     accento_list = format_accento_list()
-
-#     for lem, acc, dfn in zip(lemma_list, accento_list, definizioni_list):
-#         concatenated = f"{lem.strip()}\n\t{acc.strip()} {dfn.strip()}\n"
-#         corpus.append(concatenated)
-#         # corpus.append(format_file(concatenated))
-#     return corpus
-
-
 def create_file(file_name: str, corpus: str):
     FILE_HEADER = '#NAME "Español-Griego"\n#INDEX_LANGUAGE "Italian"\n#CONTENTS_LANGUAGE "Greek"\n'
     complete = FILE_HEADER + corpus
-    with open(f"{file_name}.dsl", "w", encoding="utf-8") as out:
+    with open(f"{file_name}", "w", encoding="utf-8") as out:
         out.write(complete)
 
 
-def main():
+def main(lemmafile, accentofile, definizionifile):
     corpus = []
-    lemma_list, accento_list, definizioni_list = read_files()
+    lemma_list, accento_list, definizioni_list = read_files(
+        lemmafile, accentofile, definizionifile
+    )
     accento_list_formatted = format_accento_list(accento_list)
     definizioni_list_formatted = format_definizione_list(definizioni_list)
     for lem, acc, dfn in zip(
@@ -83,5 +56,8 @@ def main():
 
 
 if __name__ == "__main__":
-    corpus = main()  # parse_and_join_data()
-    create_file("es-gr-dls", "".join(corpus[1:]))
+    lemmafile = "es-gr-lemma.txt"
+    accentofile = "es-gr-accento.txt"
+    definizionifile = "es-gr-definizioni.txt"
+    corpus = main(lemmafile, accentofile, definizionifile)
+    create_file("es-gr-dsl.dsl", "".join(corpus))
